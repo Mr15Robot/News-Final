7e968275d5631c93b3cf2c39c0f496d474fc4cba
Mr15Robot <155952412+Mr15Robot@users.noreply.github.com>
2024-01-15 17:17:54 +0400

    Update routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,13 @@
-
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_login import login_required, current_user
-from your_application import app, db
+from your_application import db  
 from your_application.models import Post
 from your_application.forms import PostForm
+
+app = Flask(__name__)  
+
+from your_application import db
+from your_application.models import Post
+
+
+db.create_all()
